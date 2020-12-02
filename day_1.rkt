594b79e8d2bb054b6fcbf100b604fc59590dcbf7
Simon Johnston <johnstonskj@gmail.com>
2020-12-02 11:11:14 -0800

    Added product output

--- day_1.rkt
+++ day_1.rkt
@@ -2,4 +2,7 @@
   
 (require "./expenses.rkt")
 
-(find-and-multiply (load-values "day_1_input.txt") 2020)
+(let ([result (find-and-multiply (load-values "day_1_input.txt") 2020)])
+  (if (list? result)
+      (displayln (foldl * 1 result))
+      (displayln "not found :(")))
