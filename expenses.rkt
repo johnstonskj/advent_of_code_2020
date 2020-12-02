09acefea8c95e30e3dd74f1cb97d9c0f39451037
Simon Johnston <johnstonskj@gmail.com>
2020-12-02 12:30:13 -0800

    Code cleanliness

--- expenses.rkt
+++ expenses.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
   
-(require racket/file racket/list racket/vector racket/string)
+(require racket/file racket/list racket/vector)
 (require data/heap)
 
 (provide load-values find-and-multiply)
@@ -12,36 +12,22 @@
     (map (λ (s) (heap-add! values (string->number s))) (file->lines file-name))
     (heap->vector values)))
 
-;(define (scan-and-multiply values sum with of)
-;  (let ([length (- (vector-length values) 1)])
-;    (let outer ([x 0])
-;      (when (< x length)
-;        (let* ([xv (vector-ref values x)]
-;               [yv (let inner ([y 0])
-;                     (let* ([yv (vector-ref values y)]
-;                            [csum (+ xv yv)])
-;                       (if (and (< y length) (< csum sum))
-;                           (inner (+ y 1))
-;                           yv)))]
-;               [csum (+ xv yv)])
-;          (cond  [(= csum sum) (list xv yv)]
-;                 [(< x length) (outer (+ x 1))]
-;                 [else '()]
-;                 ))
-;        ))))
-
 (define (scan-and-multiply values length sum with of)
   (let next ([x 0])
     (let* ([xv (vector-ref values x)]
            [csum (foldl + xv with)])
       (cond
+        ; got a result
         [(and (= csum sum) (= of 1)) (list* xv with)]
+        ; no result here, look at the next ranl
         [(> of 1)
-                  (let ([result (scan-and-multiply values length sum (list* xv with) (- of 1))])
-                    (if (and (empty? result) (< x (- length 1)))
-                        (next (+ x 1))
-                        result))]
+         (let ([result (scan-and-multiply values length sum (list* xv with) (- of 1))])
+           (if (and (empty? result) (< x (- length 1)))
+               (next (+ x 1))
+               result))]
+        ; no result, but wait, there's more
         [(< x (- length 1)) (next (+ x 1))]
+        ; no result, and nothing left here
         [else '()]))))
 
 (define (find-and-multiply values sum of)
