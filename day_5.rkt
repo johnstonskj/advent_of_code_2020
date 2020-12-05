4c87a7293f69a7820b42bf9e7f9a7373d357a3c3
Simon Johnston <johnstonskj@gmail.com>
2020-12-05 12:12:45 -0800

    Day 5, part two

--- day_5.rkt
+++ day_5.rkt
@@ -1,12 +1,23 @@
 #lang racket/base
 
+(require racket/list)
+
 (require "./airplane.rkt")
 
 (define ap (make-airplane 128 8))
 
 (define tickets (load-ticket-data "day_5_input.txt"))
 
-(map (λ (t) (seat-num (ticket->seat ap t))) tickets)
+(define seats (map (λ (t) (seat-num (ticket->seat ap t))) tickets))
 
 (display "max ticket number: ")
-(displayln (foldr max 0 (map (λ (t) (seat-num (ticket->seat ap t))) tickets)))
+(displayln (foldr max 0 seats))
+
+(define (seat-pairs seats)
+  (cond [(eq? seats '()) '()]
+        [(< (length seats) 2) '()]
+        [(cons (take seats 2) (seat-pairs (cdr seats)))]))
+
+(display "seat between: ")
+(displayln (filter (λ (p) (= (- (second p)  (first p)) 2)) (seat-pairs (sort seats <))))
+
