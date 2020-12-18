3612336603b7391ab9df0702f2acacfeedba333c
Simon Johnston <johnstonskj@gmail.com>
2020-12-17 20:27:19 -0800

    Day 12

--- day_12.rkt
+++ day_12.rkt
@@ -4,4 +4,6 @@
 
 (define instructions (load-navigation-instructions "day_12_input.txt"))
 
-(answer '(12 . 2) (navigate instructions) 879)
+(answer '(12 . 1) (navigate instructions) 879)
+
+(answer '(12 . 2) (navigate/with-waypoint instructions '(1 . 10)) 18107)
--- navigation.rkt
+++ navigation.rkt
@@ -4,15 +4,15 @@
 
 (require "./data.rkt")
 
-(provide load-navigation-instructions navigate manhattan-distance
-         *north* *east* *south* *west*)
+(provide load-navigation-instructions navigate navigate/with-waypoint
+         *facing-north* *facing-east* *facing-south* *facing-west*)
 
 ; ------------------------------------------------------------------------------------------
 
-(define *north* 0)
-(define *east* 90)
-(define *south* 180)
-(define *west* 270)
+(define *facing-north* 0)
+(define *facing-east* 90)
+(define *facing-south* 180)
+(define *facing-west* 270)
 
 (struct position
   (lat long facing) #:transparent)
@@ -29,107 +29,66 @@
   (+ (abs (position-lat pos))
      (abs (position-long pos))))
 
-(define (navigate instructions #:facing [facing 90])
+(define (navigate instructions #:facing [facing *facing-east*])
   (manhattan-distance (foldl nav-step (position 0 0 facing) instructions)))
 
-(define (nav-step instruction pos)
+(define (nav-step instruction ship)
   (match (car instruction)
-    [#\N
-     (position (+ (position-lat pos) (cdr instruction))
-               (position-long pos)
-               (position-facing pos))]
-    [#\S
-     (position (- (position-lat pos) (cdr instruction))
-               (position-long pos)
-               (position-facing pos))]
-    [#\E
-     (position (position-lat pos)
-               (+ (position-long pos) (cdr instruction))
-               (position-facing pos))]
-    [#\W
-     (position (position-lat pos)
-               (- (position-long pos) (cdr instruction))
-               (position-facing pos))]
-    [#\L
-     (position (position-lat pos)
-               (position-long pos) (rotate (position-facing pos)
-                                           (- (cdr instruction))))]
-    [#\R
-     (position (position-lat pos)
-               (position-long pos) (rotate (position-facing pos)
-                                           (cdr instruction)))]
+    [#\N (move-position ship (position (cdr instruction) 0 0))]
+    [#\S (move-position ship (position (- (cdr instruction)) 0 0))]
+    [#\E (move-position ship (position 0 (cdr instruction) 0))]
+    [#\W (move-position ship (position 0 (- (cdr instruction)) 0))]
+    [#\R (move-position ship (position 0 0 (cdr instruction)))]
+    [#\L (move-position ship (position 0 0 (- (cdr instruction))))]
     [#\F
      (nav-step (cons (cond 
-                       [(= (position-facing pos) *north*) #\N]
-                       [(= (position-facing pos) *east*) #\E]
-                       [(= (position-facing pos) *south*) #\S]
-                       [(= (position-facing pos) *west*) #\W])
+                       [(= (position-facing ship) *facing-north*) #\N]
+                       [(= (position-facing ship) *facing-east*) #\E]
+                       [(= (position-facing ship) *facing-south*) #\S]
+                       [(= (position-facing ship) *facing-west*) #\W])
                      (cdr instruction))
-               pos)
+               ship)
      ]))
 
-(define (rotate from by)
-  (let ([rotated (+ from by)])
-    (cond
-      [(= rotated 360) 0]
-      [(> rotated 360) (- rotated 360)]
-      [(< rotated 0) (- 360 (abs rotated))]
-      [else rotated])))
+(define (position->string pos)
+  (format "~a~a ~a~a ~a°"
+                     (abs (position-lat pos))
+                     (if (positive? (position-lat pos)) #\N #\S)
+                     (abs (position-long pos))
+                     (if (positive? (position-long pos)) #\E #\W)
+                     (position-facing pos)))
 
+(define (move-position pos by)
+  (position (+ (position-lat pos) (position-lat by))
+            (+ (position-long pos) (position-long by))
+            (modulo (+ (position-facing pos) (position-facing by)) 360)))
 
-(define (navigate/with-waypoint instructions waypoint #:facing [facing 90])
+(define (transpose-position pos by)
+  (match (if (positive? by) by (+ 360 by))
+    [(or 0 360) pos]
+    [90 (position (- (position-long pos)) (position-lat pos) (position-facing pos))]
+    [180 (position (- (position-lat pos)) (- (position-long pos)) (position-facing pos))]
+    [270 (position (position-long pos) (- (position-lat pos)) (position-facing pos))]))
+
+(define (navigate/with-waypoint instructions waypoint #:facing [facing *facing-east*])
   (manhattan-distance (car (foldl nav-step/with-waypoint
-         (cons (position 0 0 facing)
-               (position (car waypoint) (cdr waypoint) 0))
-         instructions))))
+                                  (cons (position 0 0 facing)
+                                        (position (car waypoint) (cdr waypoint) 0))
+                                  instructions))))
 
-(define (nav-step/with-waypoint instruction pos)
-  (let ([ship (car pos)] [waypoint (cdr pos)])
+(define (nav-step/with-waypoint instruction pos-pair)
+  (let ([ship (car pos-pair)] [waypoint (cdr pos-pair)])
     (match (car instruction)
-      [#\N
-       (cons ship
-             (position (+ (position-lat pos) (cdr instruction))
-                       (position-long pos)
-                       (position-facing pos)))]
-      [#\S
-       (cons ship
-             (position (- (position-lat pos) (cdr instruction))
-                       (position-long pos)
-                       (position-facing pos)))]
-      [#\E
-       (cons ship
-             (position (position-lat pos)
-                       (+ (position-long pos) (cdr instruction))
-                       (position-facing pos)))]
-      [#\W
-       (cons ship
-             (position (position-lat pos)
-                       (- (position-long pos) (cdr instruction))
-                       (position-facing pos)))]
-      [#\L
-       (position (position-lat pos)
-                 (position-long pos)
-                 (rotate/around ship
-                                (position-facing pos)
-                                (- (cdr instruction))))]
-      [#\R
-       (position (position-lat pos)
-                 (position-long pos)
-                 (rotate/around ship
-                                (position-facing pos)
-                                (cdr instruction)))]
-      [#\F
-       (nav-step (cons (cond 
-                         [(= (position-facing pos) *north*) #\N]
-                         [(= (position-facing pos) *east*) #\E]
-                         [(= (position-facing pos) *south*) #\S]
-                         [(= (position-facing pos) *west*) #\W])
-                       (cdr instruction))
-                 pos)
-       ])))
-
-(define (rotate/around pos from by)
-  0)
+      [#\N (cons ship (move-position waypoint (position (cdr instruction) 0 0)))]
+      [#\S (cons ship (move-position waypoint (position (- (cdr instruction)) 0 0)))]
+      [#\E (cons ship (move-position waypoint (position 0 (cdr instruction) 0)))]
+      [#\W (cons ship (move-position waypoint (position 0 (- (cdr instruction)) 0)))]
+      [#\R (cons ship (transpose-position waypoint (cdr instruction)))]
+      [#\L (cons ship (transpose-position waypoint (- (cdr instruction))))]
+      [#\F (cons (position (+ (position-lat ship) (* (position-lat waypoint) (cdr instruction)))
+                           (+ (position-long ship) (* (position-long waypoint) (cdr instruction)))
+                           (position-facing ship))
+                 waypoint)])))
 
 ; ------------------------------------------------------------------------------------------
 
@@ -147,9 +106,5 @@
    (test-case
     "find distance with waypoint"  
     (check-equal?
-     (car (navigate/with-waypoint '((#\F . 10) (#\N . 3) (#\F . 7) (#\R . 90) (#\F . 11))
-                                  '(1 . 10))) 286))))
-
- 
-(car (navigate/with-waypoint '((#\F . 10) (#\N . 3) (#\F . 7) (#\R . 90) (#\F . 11))
-                             '(1 . 10)))
+     (navigate/with-waypoint '((#\F . 10) (#\N . 3) (#\F . 7) (#\R . 90) (#\F . 11))
+                                  '(1 . 10))) 286)))
